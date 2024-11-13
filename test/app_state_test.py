9c0aac0cf45238c8e70b12cf8e2eea38129f28d9
Gavin <gaprice@lbl.gov>
2024-11-12 16:20:55 -0800

    Integrate KBase auth into the service.
    Next:
    * map auth errors to correct ErrorType and http codes
    * Add required roles to config and pass into auth client

---
+++ test/app_state_test.py
@@ -0,0 +1,7 @@
+# TODO TEST add tests
+
+from cdmtaskservice import app_state  # @UnusedImport
+
+
+def test_noop():
+    pass
---
+++ test/http_bearer_test.py
@@ -0,0 +1,7 @@
+# TODO TEST add tests
+
+from cdmtaskservice import http_bearer  # @UnusedImport
+
+
+def test_noop():
+    pass
