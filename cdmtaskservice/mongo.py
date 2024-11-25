ad94ed0145fb54b0b37f18436470a7ce39b175b3
Gavin <gaprice@lbl.gov>
2024-11-25 15:47:45 -0800

    On job submit look up image in DB and add to job record

--- cdmtaskservice/mongo.py
+++ cdmtaskservice/mongo.py
@@ -8,7 +8,7 @@ from motor.motor_asyncio import AsyncIOMotorDatabase
 from pymongo import IndexModel, ASCENDING, DESCENDING
 from pymongo.errors import DuplicateKeyError
 
-from cdmtaskservice.arg_checkers import not_falsy as _not_falsy
+from cdmtaskservice.arg_checkers import not_falsy as _not_falsy, require_string as _require_string
 from cdmtaskservice import models
 
 
@@ -81,6 +81,33 @@ class MongoDAO:
             else:
                 # no way to test this, but just in case
                 raise ValueError(f"Unexpected duplicate key collision for image {image}") from e
+
+    async def get_image(self, name: str, digest: str | None = None, tag: str | None = None
+    ) -> models.Image:
+        """
+        Get an image via a name and tag or digest. Either a tag or digest is required. If both
+        are provided, the tag is ignored.
+        """
+        name = _require_string(name, "name")
+        digest = digest.strip() if digest else None
+        tag = tag.strip() if tag else None
+        query = {models.FLD_IMAGE_NAME: name}
+        if digest:
+            query[models.FLD_IMAGE_DIGEST] = digest
+            err = f"with digest {digest}"
+        elif tag:
+            query[models.FLD_IMAGE_TAG] = tag
+            err = f"with tag '{tag}'"
+        else:
+            raise ValueError("A digest or tag is required")
+        doc = await self._col_images.find_one(query)
+        if not doc:
+            raise NoSuchImageError(f"No image {name} {err} exists in the system.")
+        return models.Image.model_construct(**doc)
+
+
+class NoSuchImageError(Exception):
+    """ The image does not exist in the system. """
 
 
 class ImageTagExistsError(Exception):
