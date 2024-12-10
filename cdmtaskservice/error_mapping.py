944b7a69312d46030165e8072af169358e70d902
Gavin <gaprice@lbl.gov>
2024-12-10 14:28:20 -0800

    Split job submit from job state
    The nersc / jaws flow manager has a dependency on job state as it'll
    need to get and update the state. The job submission code will have a
    dependency on the ners / jaws flow manager, and so needs to be split out
    to avoid a circular import.

--- cdmtaskservice/error_mapping.py
+++ cdmtaskservice/error_mapping.py
@@ -9,7 +9,7 @@ from cdmtaskservice.errors import ErrorType
 from cdmtaskservice.http_bearer import MissingTokenError, InvalidAuthHeaderError
 from cdmtaskservice.images import NoEntrypointError
 from cdmtaskservice.image_remote_lookup import ImageNameParseError, ImageInfoFetchError
-from cdmtaskservice.job_state import ETagMismatchError
+from cdmtaskservice.job_submit import ETagMismatchError
 from cdmtaskservice.kb_auth import InvalidTokenError, MissingRoleError
 from cdmtaskservice.mongo import (
     ImageTagExistsError,
