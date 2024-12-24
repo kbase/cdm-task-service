d4c74df279132d7671d81dacb2e8a5a9a26fe3b6
Gavin <gaprice@lbl.gov>
2024-12-24 15:22:12 -0800

    Fix bug in distributing files to containers
    Could leave containers with no files to process

--- test/models_test.py
+++ test/models_test.py
@@ -1,7 +1,46 @@
-# TODO TEST add tests
+# TODO TEST add  more tests
 
 from cdmtaskservice import models  # @UnusedImport
 
 
-def test_noop():
-    pass
+def test_files_per_container():
+    _files_per_container(1, 1, 1, [[1]])
+    _files_per_container(1, 2, 1, [[1, 2]])
+    _files_per_container(2, 1, 1, [[1]])
+    _files_per_container(2, 2, 2, [[1], [2]])
+    _files_per_container(3, 2, 2, [[1], [2]])
+    _files_per_container(3, 3, 3, [[1], [2], [3]])
+    _files_per_container(3, 4, 3, [[1, 2], [3], [4]])
+    _files_per_container(3, 5, 3, [[1, 2], [3, 4], [5]])
+    _files_per_container(3, 6, 3, [[1, 2], [3, 4], [5, 6]])
+    _files_per_container(3, 7, 3, [[1, 2, 3], [4, 5], [6, 7]])
+
+
+def _files_per_container(containers: int, files: int, expcont: int, expfiles: list[list[int]]):
+    ji = models.JobInput(
+        cluster=models.Cluster.PERLMUTTER_JAWS,
+        image="fakeimage",
+        params=models.Parameters(),
+        num_containers=containers,
+        input_files=[f"foo/bar{i}" for i in range(1, files + 1)],
+        output_dir="foo/bar"
+    )
+    fpc = ji.get_files_per_container()
+    exp = []
+    for lst in expfiles:
+        exp.append([f"foo/bar{i}" for i in lst])
+    assert fpc == models.FilesPerContainer(expcont, exp)
+    
+    ji = models.JobInput(
+        cluster=models.Cluster.PERLMUTTER_JAWS,
+        image="fakeimage",
+        params=models.Parameters(),
+        num_containers=containers,
+        input_files=[models.S3File(file=f"foo/bar{i}") for i in range(1, files + 1)],
+        output_dir="foo/bar"
+    )
+    fpc = ji.get_files_per_container()
+    exp = []
+    for lst in expfiles:
+        exp.append([models.S3File(file=f"foo/bar{i}") for i in lst])
+    assert fpc == models.FilesPerContainer(expcont, exp)
