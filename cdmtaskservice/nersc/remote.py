1896e19903cb9d571781b454abace14b89b70073
Gavin <gaprice@lbl.gov>
2025-01-07 15:10:50 -0800

    Handle errors during the remote up/download process

--- cdmtaskservice/nersc/remote.py
+++ cdmtaskservice/nersc/remote.py
@@ -9,9 +9,10 @@ python versions.
 import asyncio
 import hashlib
 import json
-import logging
 import os
 import requests
+import sys
+import traceback
 from typing import Any
 
 from cdmtaskservice.s3.remote import process_data_transfer_manifest as s3_pdtm
@@ -30,6 +31,7 @@ def _generate_md5s(path: str, files: list[dict[str, Any]]):
 
 def process_data_transfer_manifest(
         manifest_file_path: str,
+        result_file_path: str,
         callback_url: str,
         md5_json_file_path: str = None,
     ):
@@ -44,30 +46,57 @@ def process_data_transfer_manifest(
     # its structure.
     # Similarly, it should only be produced and consumed by the service, and so we don't
     # stress error checking too much.
-    # TODO TEST add tests for this and its dependency functions, including logging
+    # TODO TEST add tests for this and its dependency functions
     # Potential performance improvement could include a shared cross job cache for files
     #    only useful if jobs are reusing the same files, which seems def possible
-    # TODO LOGGINNG logging doesn't work
-    # TODO IMPORTANT ERRORHANDLING write an output file that can be read by the CTS
-    #                sfapi tasks only last for 10 minutes after completions
-    log = logging.getLogger(__name__)
+    jex = None
+    jext = None
+    cex = None
+    cext = None
+    ctext = None
+    ret = None
     try:
         with open(manifest_file_path) as f:
             manifest = json.load(f)
         if md5_json_file_path:  # assume that this is only present for uploads
             _generate_md5s(md5_json_file_path, manifest["file-transfers"]["files"])
         asyncio.run(s3_pdtm(manifest["file-transfers"]))
+    except Exception as jobexep:
+        jex = jobexep
+        jext = traceback.format_exc()
     finally:
-        log.info(f"Pinging callback url {callback_url}")
-        ret = requests.get(callback_url)
+        try:
+            # may want some retries here, halting on incorrect job state messages
+            ret = requests.get(callback_url)
+        except Exception as e:
+            cex = e
+            cext = traceback.format_exc()
         if ret.status_code < 200 or ret.status_code > 299:
-            log.error(ret.text)
-        ret.raise_for_status()
+            ctext = ret.text
+    with open(result_file_path, "w") as f:
+        if jex or cex or ctext:
+            j = {
+                "result": "fail",
+                "job_msg": str(jex) if jex else None,
+                "job_trace": jext,
+                # callback error info is mostly for debugging since the service won't be pinged
+                # if the callback fails
+                "callback_msg": str(cex) if cex else None,
+                "callback_trace": cext,
+                "callback_text": ctext,
+                "callback_code": ret.status_code if ret else None,
+                "callback_url": callback_url,
+            }
+            json.dump(j, f, indent=4)
+            sys.exit(1)
+        else:
+            json.dump({"result": "success"}, f)
 
 
 if __name__ == "__main__":
     process_data_transfer_manifest(
         os.environ["CTS_MANIFEST_LOCATION"],
+        os.environ["CTS_RESULT_FILE_LOCATION"],
         os.environ["CTS_CALLBACK_URL"],
-        os.environ.get("CTS_MD5_FILE_LOCATION")
+        os.environ.get("CTS_MD5_FILE_LOCATION"),
     )
