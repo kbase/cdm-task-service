feb7a3702ef2081cc40553d3e78bbb82fd0ff6f9
Gavin <gaprice@lbl.gov>
2025-01-08 14:50:22 -0800

    Error out if num_containers > len(input_files)

--- test/models_test.py
+++ test/models_test.py
@@ -1,22 +1,23 @@
 # TODO TEST add  more tests
 
+from pydantic import ValidationError
+from pytest import raises
+
 from cdmtaskservice import models  # @UnusedImport
 
 
 def test_files_per_container():
-    _files_per_container(1, 1, 1, [[1]])
-    _files_per_container(1, 2, 1, [[1, 2]])
-    _files_per_container(2, 1, 1, [[1]])
-    _files_per_container(2, 2, 2, [[1], [2]])
-    _files_per_container(3, 2, 2, [[1], [2]])
-    _files_per_container(3, 3, 3, [[1], [2], [3]])
-    _files_per_container(3, 4, 3, [[1, 2], [3], [4]])
-    _files_per_container(3, 5, 3, [[1, 2], [3, 4], [5]])
-    _files_per_container(3, 6, 3, [[1, 2], [3, 4], [5, 6]])
-    _files_per_container(3, 7, 3, [[1, 2, 3], [4, 5], [6, 7]])
+    _files_per_container(1, 1, [[1]])
+    _files_per_container(1, 2, [[1, 2]])
+    _files_per_container(2, 2, [[1], [2]])
+    _files_per_container(3, 3, [[1], [2], [3]])
+    _files_per_container(3, 4, [[1, 2], [3], [4]])
+    _files_per_container(3, 5, [[1, 2], [3, 4], [5]])
+    _files_per_container(3, 6, [[1, 2], [3, 4], [5, 6]])
+    _files_per_container(3, 7, [[1, 2, 3], [4, 5], [6, 7]])
 
 
-def _files_per_container(containers: int, files: int, expcont: int, expfiles: list[list[int]]):
+def _files_per_container(containers: int, files: int, expfiles: list[list[int]]):
     ji = models.JobInput(
         cluster=models.Cluster.PERLMUTTER_JAWS,
         image="fakeimage",
@@ -29,7 +30,7 @@ def _files_per_container(containers: int, files: int, expcont: int, expfiles: li
     exp = []
     for lst in expfiles:
         exp.append([f"foo/bar{i}" for i in lst])
-    assert fpc == models.FilesPerContainer(expcont, exp)
+    assert fpc == exp
     
     ji = models.JobInput(
         cluster=models.Cluster.PERLMUTTER_JAWS,
@@ -43,4 +44,18 @@ def _files_per_container(containers: int, files: int, expcont: int, expfiles: li
     exp = []
     for lst in expfiles:
         exp.append([models.S3File(file=f"foo/bar{i}") for i in lst])
-    assert fpc == models.FilesPerContainer(expcont, exp)
+    assert fpc == exp
+
+
+def test_job_input_fail_too_many_containers():
+    err = ("1 validation error for JobInput\n  Value error, num_containers must be less than or "
+           + "equal to the number of input files")
+    with raises(ValidationError, match=err):
+        models.JobInput(
+            cluster=models.Cluster.PERLMUTTER_JAWS,
+            image="fakeimage",
+            params=models.Parameters(),
+            num_containers=3,
+            input_files=[f"foo/bar{i}" for i in range(1, 3)],
+            output_dir="foo/bar"
+    )
