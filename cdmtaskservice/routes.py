5b58d536ec96f82d4ac1432d11896dd636b5782a
bio-boris <bio-boris@users.noreply.github.com>
2025-02-05 11:38:04 -0600

    Update routes.py

--- cdmtaskservice/routes.py
+++ cdmtaskservice/routes.py
@@ -86,6 +86,10 @@ async def root() -> Root:
     }
 
 
+from fastapi import Request, Depends
+from pydantic import BaseModel, Field
+from typing import Annotated, Dict
+
 class WhoAmI(BaseModel):
     """ The username associated with the provided user token and the user's admin state. """
     user: Annotated[str, Field(example="kbasehelp", description="The user's username.")]
