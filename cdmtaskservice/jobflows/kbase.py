kbase/cdm-task-service

commit 8f024372e255c57a9ee83014c630d209cbb46816

Make exit codes available regardless of flow state
Also fix an embarrassing bug

cdmtaskservice/jobflows/kbase.py
@@ -145,12 +145,6 @@ class KBaseRunner(JobFlow):
             return await self._mongo.get_subjob(job_id, container_num)
         return await self._mongo.get_subjobs(job_id)
 
-    async def get_exit_codes(self, job: models.Job) -> list[int | None]:
-        """
-        Get the exit codes of the containers / subjobs for a job.
-        """
-        return await self._mongo.get_exit_codes_for_subjobs(_not_falsy(job, "job").id)
-
     async def get_job_external_runner_status(
         self,
         job: models.AdminJobDetails,

cdmtaskservice/job_state.py
@@ -280,6 +280,25 @@ class JobState:
         )
         return job
     
+    async def get_job_exit_codes(self, job_id: str, user: CTSUser, as_admin: bool = False
+    ) -> list[int | None]:
+        """
+        Get the container exit codes for a job.
+        
+        job_id - the job ID.
+        user - the user requesting the exit codes.
+        as_admin - True if the user should always have access to the job.
+        
+        Returns a list of the exit codes, some or all of which may be None if the container hasn't
+        exited.
+        """
+        job = await self.get_job(job_id, user, as_admin)
+        containers_self_managed = sites.CLUSTER_TO_EXECUTION_TYPE[job.job_input.cluster]
+        if containers_self_managed:
+            return await self._mongo.get_exit_codes_for_subjobs(job.id)
+        ecs = await self._mongo.get_exit_codes_for_standard_job(job.id)
+        return ecs if ecs else [None] * job.job_input.num_containers
+    
     async def stream_job_logs(
         self,
         job_id: str,

cdmtaskservice/jobflows/nersc_jaws.py
@@ -166,13 +166,6 @@ class NERSCJAWSRunner(JobFlow):
             f"This method is not supported for the {self.CLUSTER.value} job flow"
         )
 
-    async def get_exit_codes(self, job: models.Job) -> list[int | None]:
-        """
-        Get the exit codes of the containers / subjobs for a job.
-        """
-        ecs = await self._mongo.get_exit_codes_for_standard_job(_not_falsy(job, "job").id)
-        return ecs if ecs else [None] * job.job_input.num_containers
-
     async def get_job_external_runner_status(
         self,
         job: models.AdminJobDetails,
@@ -359,7 +352,7 @@ class NERSCJAWSRunner(JobFlow):
     async def _upload_complete(self, job: models.AdminJobDetails):
         try:
             await self._mongo.save_exit_codes_for_standard_job(
-                job.id, [8] * job.job_input.num_containers
+                job.id, [0] * job.job_input.num_containers
             )
             checksums = await self._nman.get_uploaded_JAWS_files(job)
             if not checksums:

cdmtaskservice/routes.py
@@ -335,10 +335,8 @@ async def get_job_exit_codes(
     job_id: _ANN_JOB_ID,
     user: CTSUser=Depends(_AUTH),
 ) -> ExitCodes:
-    appstate = app_state.get_app_state(r)
-    job = await appstate.job_state.get_job(job_id, user)
-    flow = await appstate.jobflow_manager.get_flow(job.job_input.cluster)
-    return ExitCodes(exit_codes=await flow.get_exit_codes(job))
+    jobstate = app_state.get_app_state(r).job_state
+    return ExitCodes(exit_codes=await jobstate.get_job_exit_codes(job_id, user))
 
 
 _ANN_CONTAINER_NUMBER = Annotated[int, FastPath(
@@ -688,10 +686,8 @@ async def get_job_exit_codes_admin(
     user: CTSUser=Depends(_AUTH),
 ) -> ExitCodes:
     _ensure_admin(user, "Only service administrators can view other users' job details.")
-    appstate = app_state.get_app_state(r)
-    job = await appstate.job_state.get_job(job_id, user, as_admin=True)
-    flow = await appstate.jobflow_manager.get_flow(job.job_input.cluster)
-    return ExitCodes(exit_codes=await flow.get_exit_codes(job))
+    jobstate = app_state.get_app_state(r).job_state
+    return ExitCodes(exit_codes=await jobstate.get_job_exit_codes(job_id, user, as_admin=True))
 
 
 class SubJobs(BaseModel):
@@ -740,6 +736,8 @@ async def _get_containers(
     user: CTSUser,
     container_num: int | None = None,
 ) -> models.SubJob | list[models.SubJob]:
+    # TODO CODE could use same strategy as exit codes to allow getting state w/o flow availble.
+    #           Since admin only don't worry about it for now
     _ensure_admin(user, "Only service administrators get container state.")
     appstate = app_state.get_app_state(r)
     job = await appstate.job_state.get_job(job_id, user, as_admin=True)

cdmtaskservice/sites.py
@@ -102,6 +102,18 @@ CLUSTER_TO_SITE = {
 """ A mapping of compute clusters to their site information. """
 
 
+CLUSTER_TO_EXECUTION_TYPE = {
+    Cluster.PERLMUTTER_JAWS: False,
+    Cluster.LAWRENCIUM_JAWS: False,
+    Cluster.KBASE: True,
+}
+"""
+A mapping of compute clusters to their job container management type.
+
+True - managed by this service
+False - managed by an external service (e.g. JAWS)
+"""
+
 MAX_CPUS = max([cl.cpus_per_node for cl in CLUSTER_TO_SITE.values()])
 """
 The maximum number of cpus that can be requested for a container across all clusters.

commit 1d05060061392a2cbb9d0522813c1f0f9b361faa

Fix regression in handling errored KBase jobs
Regression from 8f024372e255c57a9ee83014c630d209cbb46816 where
get_exit_codes was moved from the kbase job flow to job_state

cdmtaskservice/jobflows/kbase.py
@@ -319,7 +319,7 @@ class KBaseRunner(JobFlow):
 
     async def _error_job(self, job: models.AdminJobDetails):
         cpu_hours, cpu_factor, max_mem = await self._get_condor_stats(job)
-        exit_codes = await self.get_exit_codes(job)
+        exit_codes = await self._mongo.get_exit_codes_for_subjobs(job.id)
         # if any exit codes are present and > 0, a container failed. Exit codes can be None
         # if the container never ran
         err_exit = set(exit_codes) - {0, None}